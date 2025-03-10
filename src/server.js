8ee6ce967fcd3a750ec5b12e085d49ee62a68586
Neill Lewis <zneill@gmail.com>
2025-03-10 01:40:37 -0600

    more redis troubleshooting

--- src/server.js
+++ src/server.js
@@ -4,5 +4,6 @@ const logger = require('./utils/logger');
 // Only start the HTTP server if NOT running in AWS Lambda
 if (!process.env.LAMBDA_TASK_ROOT) {
   const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => logger.info(`API Gateway running on port ${PORT}`));
+  const REDIS_URL = process.env.REDIS_URL || 'banana'
+  app.listen(PORT, () => logger.info(`API Gateway running on port ${PORT} and redis is hosting from ${REDIS_URL}`));
 }
