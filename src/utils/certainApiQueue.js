973a9bbf520711a8702aa860778a5e7390bb99f4
Neill Lewis <zneill@gmail.com>
2025-03-17 19:43:11 -0600

    redis troubleshooting

--- src/utils/certainApiQueue.js
+++ src/utils/certainApiQueue.js
@@ -12,8 +12,8 @@ const CERTAIN_API_PASSWORD = decrypt(process.env.CERTAIN_API_PASSWORD_ENCRYPTED)
 // Create a Bull queue using the Redis endpoint
 const redisUrl = process.env.REDIS_URL;
 const redisClient = new Redis(redisUrl);
-redisclient.on('connect', () => logger.info('✅ connected to redis.'));
-redisclient.on('error', (err) => logger.error(`❌ redis error: ${err.message}`));
+redisClient.on('connect', () => logger.info('✅ connected to redis.'));
+redisClient.on('error', (err) => logger.error(`❌ redis error: ${err.message}`));
 const certainApiQueue = new Queue('certainApiQueue', { 
     redis: { client: redisClient }
 });
